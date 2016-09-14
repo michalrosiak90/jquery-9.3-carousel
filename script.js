0f31e070487858340b8c01442889bddc362c6134
Michal Rosiak <michalrosiak90@gmail.com>
2016-09-14 20:02:20 +0200

    Dodanie plikow html, style i script do repozytorium zdalnego

---
+++ script.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	var carouselList = $("#carousel ul");
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+	function changeSlide() {
+		carouselList.animate({marginLeft:-400}, 700, moveFirstSlide);
+	};
+	setInterval(changeSlide, 3500);
+
+});
